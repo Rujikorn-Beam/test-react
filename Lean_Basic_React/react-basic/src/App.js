84eee7c69fdceaaccc5be949a76b463dfb500acf
Rujikorn-Beam <bigbeem006@gmail.com>
2021-08-18 01:37:03 +0700

    update

--- Lean_Basic_React/react-basic/src/App.js
+++ Lean_Basic_React/react-basic/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //  section/div/React.Fragment/ <>
+    <React.Fragment> 
+      <article> 
+        <h1>Program บัญชีรายรับ - รายจ่าย</h1>
+        <p>บันทึกข้อมูลบัญชีในแต่ละวัน</p>
+        <ul>
+          <li>ค่าเดินทาง <span>200</span></li>
+          <li>เงินเดือน <span>2000</span></li>
+          <li>อาหาร <span>500</span></li>
+        </ul>
+      </article>
+    </React.Fragment>
+
   );
 }
 
--- Lean_Basic_React/react-basic/src/index.js
+++ Lean_Basic_React/react-basic/src/index.js
@@ -22,7 +22,7 @@ class Hello2Component extends React.Component{
 }
 
 
-ReactDOM.render(<FileHelloComponent/>,document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
